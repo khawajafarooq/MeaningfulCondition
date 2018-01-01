f921fce7e9a0c3b73f0ae1d4a1cb494c42dcd0af
Khawaja Farooq <kfarooqa@gmail.com>
2018-01-01 20:12:36 +0300

    adding example

--- MeaningfulConditions.playground/Contents.swift
+++ MeaningfulConditions.playground/Contents.swift
@@ -3,5 +3,43 @@
 import UIKit
 
 
+// condition
+
+struct VendingItem {
+    let price: Int
+    let quantity: Int
+}
+
+extension VendingItem {
+    
+    var isValidItem: Bool {
+        return (self.price > 0 && self.quantity > 0)
+    }
+}
+
+
+func vend(for item: VendingItem) {
+    
+    // Usual way
+    /*
+     if vendingItem.price > 0 && vendingItem.quantity > 0 {
+     print("Vend it 💵")
+     } else {
+     print("Invalid item 🚫")
+     }
+     */
+    
+    // swifty way
+    if vendingItem.isValidItem {
+        print("Vending it 💵")
+    } else {
+        print("Invalid item 🚫")
+    }
+}
+
+let vendingItem = VendingItem(price: 10, quantity: 2)
+
+vend(for: vendingItem)
+
 
 
